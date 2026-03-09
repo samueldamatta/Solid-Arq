8f57e03decc228eb5ce4a901f73f7bf13ab11ab6
samueldamatta <samueldamattaid@gmail.com>
2026-03-08 23:02:29 -0300

    :hammer: refactor:user repository structure: move PrismaUsersRepository to a new directory, implement UsersRepository interface, and update register use case to use the new repository structure.

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,6 +1,6 @@
 import { prisma } from "../lib/prisma.js"
 import { hash } from "bcryptjs"
-import { PrismaUsersRepository } from "../repositories/prisma-users-repository.js"
+import type { UsersRepository } from "../repositories/users-repository.js"
 
 interface RegisterUseCaseRequest {
     name: string
@@ -10,17 +10,13 @@ interface RegisterUseCaseRequest {
 
 export class RegisterUseCase {
 
-    constructor(private usersRepository: any){}
+    constructor(private usersRepository: UsersRepository){}
 
     async execute({ name, email, password }: RegisterUseCaseRequest) {
 
         const password_hash = await hash(password, 6)
-    
-        const userWithSameEmail = await prisma.user.findUnique({
-            where: {
-                email,
-            },
-        })
+
+        const userWithSameEmail = await this.usersRepository.findByEmail(email)
     
         if (userWithSameEmail) {
             throw new Error("User already exists")
