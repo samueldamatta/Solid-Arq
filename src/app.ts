4df25f1ea60437597270085c751aeb7f9282cc7c
samueldamatta <samueldamattaid@gmail.com>
2026-03-12 19:23:54 -0300

    :sparkles: feat: enhance error handling in app: add global error handler for validation and internal errors in Fastify application, and remove version declaration from docker-compose.yml.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,24 @@
 import fastify from "fastify";
 import { appRoutes } from "./http/controllers/routes.js";
-
+import { UserAlreadyExistsError } from "./use-cases/erros/user-already-exists-error.js";
+import { ZodError } from "zod";
+import { env } from "./env/index.js";
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+    if (error instanceof ZodError) {
+        return reply
+        .status(400)
+        .send({ message: 'Validation error.', issues: error.format() })
+    }
+
+    if (env.NODE_ENV !== 'production') {
+        console.error(error)
+    }else {
+        // LOG para uma ferramenta externa
+    }
+
+    return reply.status(500).send({ message: 'Internal server error.' })
+})
--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -28,7 +28,7 @@ export async function register (request: FastifyRequest, reply: FastifyReply){
         if (error instanceof UserAlreadyExistsError) {
             return reply.status(409).send({ message: error.message })
         }
-        return reply.status(500).send({ message: 'Internal server error' })
+        throw error
     }
 
     return reply.status(201).send()
